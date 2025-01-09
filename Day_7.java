f94adc1e02cb0b27b42098cf701f7266cd7af8a9
Meenu-2005 <2311cs010466@mallareddyuniversity.ac.in>
2025-01-09 10:01:16 +0530

    Add files via upload

---
+++ Day_7.java
@@ -0,0 +1,18 @@
+public class Day_7{
+    public static void main(String[] args)
+    {
+
+        int[] result = new int[2];
+        for(int i=0;i<arr.length;i++){
+            for(int j =i+1;j<arr.length;j++){
+                if(arr[i]+arr[j] == target){
+                    result[0] =1;
+
+                    result[1] =j;
+                }
+
+            }
+        }
+
+    }
+}
