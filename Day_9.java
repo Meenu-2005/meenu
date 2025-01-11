7182132f87b7612cd1c74f5c95cbcbf472820dcd
Meenu-2005 <2311cs010466@mallareddyuniversity.ac.in>
2025-01-11 11:45:44 +0530

    Add files via upload

---
+++ Day_9.java
@@ -0,0 +1,18 @@
+public class Day_9{
+        String name;
+        int age;
+        int id;
+        String branch;
+        public static void main(String[] args){
+        Day_9 s1=new Day_9();
+        s1.name=" mani";
+        s1.id=308;
+        s1.age=19;
+        s1.branch="cse";
+        System.out.println(s1.name);
+       
+
+
+    }
+    
+}
